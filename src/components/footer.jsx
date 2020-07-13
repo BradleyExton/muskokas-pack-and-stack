59c46a554584313e5ffda4cb9104598bc9caaa9d
Bradley Exton <bradleyexton@MacBook-Pro.local>
2020-07-13 09:55:42 -0400

    add footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__text">Bradley Exton 2020</div>
+    </div>
+  )
+}
+
+export default footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Navigation from "./navigation"
+import Footer from "./footer"
 
 const layout = ({ children }) => {
   return (
     <div className="layout">
       <Navigation className="layout__navigation" />
       <main className="layout__content">{children}</main>
+      <Footer />
     </div>
   )
 }
