e48655966d0d9f8dea560a0f16723236727c9a74
Bradley Exton <bradleyexton@MacBook-Pro.local>
2020-07-15 13:16:16 -0400

    add image render conditionals to gatsby images to prevent null build error

--- src/components/landingContent.jsx
+++ src/components/landingContent.jsx
@@ -19,38 +19,42 @@ const LandingContent = () => {
           }
         }
       `}
-      render={data => (
-        <BackgroundImage
-          className="landingContent"
-          fluid={data.road.childImageSharp.fluid}
-        >
-          <div className="card">
-            <div className="card__icon">
-              <Checkmark />
+      render={data =>
+        data.road ? (
+          <BackgroundImage
+            className="landingContent"
+            fluid={data.road.childImageSharp.fluid}
+          >
+            <div className="card">
+              <div className="card__icon">
+                <Checkmark />
+              </div>
+              <div className="card__title">RELIABLE</div>
+              <div className="card__text">
+                Our Movers are trained to take excelent care of your belongings.
+              </div>
             </div>
-            <div className="card__title">RELIABLE</div>
-            <div className="card__text">
-              Our Movers are trained to take excelent care of your belongings.
+            <div className="card">
+              <div className="card__icon">
+                <Coin />
+              </div>
+              <div className="card__title">AFFORDABLE</div>
+              <div className="card__text">Rates as low as $89.95hr</div>
             </div>
-          </div>
-          <div className="card">
-            <div className="card__icon">
-              <Coin />
+            <div className="card">
+              <div className="card__icon">
+                <Location />
+              </div>
+              <div className="card__title">LOCAL</div>
+              <div className="card__text">
+                Our moving company is located in the heart of Muskoka
+              </div>
             </div>
-            <div className="card__title">AFFORDABLE</div>
-            <div className="card__text">Rates as low as $89.95hr</div>
-          </div>
-          <div className="card">
-            <div className="card__icon">
-              <Location />
-            </div>
-            <div className="card__title">LOCAL</div>
-            <div className="card__text">
-              Our moving company is located in the heart of Muskoka
-            </div>
-          </div>
-        </BackgroundImage>
-      )}
+          </BackgroundImage>
+        ) : (
+          <div />
+        )
+      }
     ></StaticQuery>
   )
 }
--- src/components/landingHeader.jsx
+++ src/components/landingHeader.jsx
@@ -16,21 +16,25 @@ const LandingHeader = () => {
           }
         }
       `}
-      render={data => (
-        <BackgroundImage
-          className="landingHeader"
-          fluid={data.lakeMuskoka.childImageSharp.fluid}
-          preserveStackingContext
-        >
-          <h1 className="landingHeader__text">
-            <span className="landingHeader__text--main">muskoka's top</span>
-            <span className="landingHeader__text--sub">movers</span>
-          </h1>
-          <Link to="/quote" className="landingHeader__button">
-            Get a Quote
-          </Link>
-        </BackgroundImage>
-      )}
+      render={data =>
+        data.lakeMuskoka ? (
+          <BackgroundImage
+            className="landingHeader"
+            fluid={data.lakeMuskoka.childImageSharp.fluid}
+            preserveStackingContext
+          >
+            <h1 className="landingHeader__text">
+              <span className="landingHeader__text--main">muskoka's top</span>
+              <span className="landingHeader__text--sub">movers</span>
+            </h1>
+            <Link to="/quote" className="landingHeader__button">
+              Get a Quote
+            </Link>
+          </BackgroundImage>
+        ) : (
+          <div />
+        )
+      }
     ></StaticQuery>
   )
 }
